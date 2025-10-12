1f0b185ccbb1d806b799963ddf0deac391a2cf35
daniel-konoplanski <85637752+daniel-konoplanski@users.noreply.github.com>
2025-10-12 16:44:54 +0200

    Modified the work guard example

--- src/chapter_9/background_task/main.cpp
+++ src/chapter_9/background_task/main.cpp
@@ -5,36 +5,73 @@
 #include <boost/asio/steady_timer.hpp>
 #include <boost/system/detail/error_code.hpp>
 
+#include <functional>
 #include <print>
 #include <thread>
+#include <iostream>
 
 using namespace std::chrono_literals;
 
 void backgroundTask(boost::asio::io_context& ioContext)
 {
-    std::this_thread::sleep_for(2s);
-
     std::print("Posting background task\n");
 
     auto performWork = []() {
-        std::this_thread::sleep_for(1s);
         std::print("Doing some work\n");
-        std::this_thread::sleep_for(1s);
     };
 
     boost::asio::post(ioContext, performWork);
 }
 
+// int main()
+// {
+//     boost::asio::io_context ioContext{};
+
+//     auto jt1 = std::jthread(
+//         [](boost::asio::io_context& io) {
+//             backgroundTask(io);
+//         },
+//         std::ref(ioContext));
+
+//     auto jt2 = std::jthread(
+//         [](boost::asio::io_context& io) {
+//             std::print("Adding worker guard\n");
+//             auto workGuard = boost::asio::make_work_guard(io);
+//             std::this_thread::sleep_for(10s);
+//             std::print("Resetting worker guard\n");
+//             workGuard.reset();
+//         },
+//         std::ref(ioContext));
+
+//     ioContext.run();
+
+//     return 0;
+// }
+
 int main()
 {
-    boost::asio::io_context ioContext{};
+    boost::asio::io_context io_context;
 
-    auto workGuard = boost::asio::make_work_guard(ioContext);
+    auto work_guard = boost::asio::make_work_guard(
+        io_context);
 
-    backgroundTask(ioContext);
-    ioContext.run();
+    std::thread io_thread([&io_context]() {
+        std::cout << "Running io_context.\n";
+        io_context.run();
+        std::cout << "io_context stopped.\n";
+    });
 
-    workGuard.reset();
+    std::thread worker(
+        backgroundTask,
+        std::ref(io_context));
+
+    // Main thread doing some work.
+    std::this_thread::sleep_for(5s);
+    std::cout << "Removing work_guard." << std::endl;
+
+    work_guard.reset();
+    worker.join();
+    io_thread.join();
 
     return 0;
 }
