7b92632b46a993b37a6cb268801514dced8bd6f0
daniel-konoplanski <85637752+daniel-konoplanski@users.noreply.github.com>
2025-10-27 22:41:41 +0100

    Added stop handling "As an exercise, try to implement a stop() function that breaks the cycle between do_read() and do_write()"

--- src/chapter_9/echo_server/main.cpp
+++ src/chapter_9/echo_server/main.cpp
@@ -1,13 +1,18 @@
 #include <array>
-#include <boost/asio/write_at.hpp>
+#include <atomic>
+#include <csignal>
 #include <cstddef>
 #include <cstdint>
 #include <iostream>
 #include <memory>
+#include <print>
+#include <vector>
 
 #include <boost/asio.hpp>
+#include <boost/asio/signal_set.hpp>
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/write_at.hpp>
 #include <boost/system/detail/error_code.hpp>
 
 using namespace std::chrono_literals;
@@ -20,11 +25,20 @@ class Session : public std::enable_shared_from_this<Session>
 public:
     Session(tcp::socket socket)
         : socket_(std::move(socket))
-    {}
+    {
+    }
 
     void start()
     {
         do_read();
+    }
+
+    void stop()
+    {
+        should_stop_.store(true);
+        boost::system::error_code ec;
+        socket_.shutdown(tcp::socket::shutdown_both, ec);
+        socket_.close(ec);
     }
 
 private:
@@ -36,22 +50,26 @@ private:
             boost::asio::buffer(data_, max_length),
             [this, self](boost::system::error_code ec, std::size_t length)
             {
-                if (!ec) do_write(length);
+                if (!ec && !should_stop_.load()) do_write(length);
             });
     }
 
     void do_write(uint32_t length)
     {
-        auto self{ shared_from_this() };
-        boost::asio::async_write(socket_, boost::asio::buffer(data_, length),
-        [this, self](boost::system::error_code ec, std::size_t) {
-            if (!ec) do_read();
-        });
+        auto self(shared_from_this());
+        boost::asio::async_write(
+            socket_,
+            boost::asio::buffer(data_, length),
+            [this, self](boost::system::error_code ec, std::size_t)
+            {
+                if (!ec && !should_stop_.load()) do_read();
+            });
     }
 
 private:
-    static constexpr uint32_t max_length{ 1024 };
-    tcp::socket socket_;
+    static constexpr uint32_t    max_length{ 1024 };
+    std::atomic<bool>            should_stop_{};
+    tcp::socket                  socket_;
     std::array<char, max_length> data_;
 };
 
@@ -63,28 +81,40 @@ public:
     {
         tcp::endpoint endpoint(tcp::v4(), port_number);
         acceptor_ = std::make_shared<tcp::acceptor>(tcp::acceptor(*ioContextPtr_, endpoint));
-
         do_accept();
+    }
+
+    void stop()
+    {
+        acceptor_->close();
+        for (auto& session : sessions_)
+        {
+            session->stop();
+        }
+        sessions_.clear();
     }
 
 private:
     void do_accept()
     {
-        acceptor_->async_accept([this](boost::system::error_code ec, tcp::socket socket) {
-            if (!ec)
+        acceptor_->async_accept(
+            [this](boost::system::error_code ec, tcp::socket socket)
             {
-                auto session = std::make_shared<Session>(std::move(socket));
-                session->start();
-
-                do_accept();
-            }
-        });
+                if (!ec)
+                {
+                    auto session = std::make_shared<Session>(std::move(socket)); // shared_ptr!
+                    sessions_.push_back(session);
+                    session->start();
+                    do_accept();
+                }
+            });
     }
 
 private:
-    constexpr static uint32_t port_number{ 1234 };
-    IoContextPtr              ioContextPtr_;
-    TcpAcceptorPtr            acceptor_;
+    constexpr static uint32_t             port_number{ 1234 };
+    IoContextPtr                          ioContextPtr_;
+    TcpAcceptorPtr                        acceptor_;
+    std::vector<std::shared_ptr<Session>> sessions_;
 };
 
 int main()
@@ -96,6 +126,14 @@ int main()
     try
     {
         EchoServer server(io);
+
+        boost::asio::signal_set signals{*io, SIGINT, SIGTERM};
+        signals.async_wait([&server, io](auto, auto) {
+            std::print("\nShutting down server...\n");
+            server.stop();
+            io->stop();
+        });
+
         io->run();
     }
     catch (std::exception& e)
