30862f37bd10aa47e1478b49f1e84dcd6a495a0d
daniel-konoplanski <85637752+daniel-konoplanski@users.noreply.github.com>
2025-11-01 18:09:39 +0100

    Added signals example

---
+++ src/chapter_9/signals/main.cpp
@@ -0,0 +1,43 @@
+#include <boost/asio.hpp>
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/signal_set.hpp>
+#include <csignal>
+#include <iostream>
+
+using namespace std::chrono_literals;
+
+int main()
+{
+    try
+    {
+        boost::asio::io_context io_context;
+
+        boost::asio::signal_set signals(io_context, SIGINT, SIGTERM);
+
+        auto handle_signal = [&](const boost::system::error_code& ec, int signal)
+        {
+            if (!ec)
+            {
+                std::cout << "Signal received: " << signal << std::endl;
+
+                // Code to perform cleanup or shutdown.
+                io_context.stop();
+            }
+        };
+
+        signals.async_wait(handle_signal);
+
+        std::cout << "Application is running. "
+                  << "Press Ctrl+C to stop...\n";
+
+        io_context.run();
+
+        std::cout << "Application has exited cleanly.\n";
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << '\n';
+    }
+
+    return 0;
+}
